8f275401963d937db017d746c49e0cb760e98357
Marko Klemetti <marko.klemetti@gmail.com>
2018-02-06 10:59:42 +0200

    Update styles

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {/* TODO */1}
       </button>
     );
   }
@@ -24,17 +24,13 @@ class Board extends React.Component {
     return (
       <div>
         <div className="status">{status}</div>
-        <div className="board-row">
+        <div className="board">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
